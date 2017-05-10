ef801c9b2e9c2f687acf786fd64fbe8cd56db65c
msdeep14 <mandeepsinghshekhawat95@gmail.com>
2017-05-10 13:54:47 +0530

    fix code structure

--- 2048.cpp
+++ 2048.cpp
@@ -10,8 +10,7 @@ using namespace std;
 int score=0;
 int undo_flag=0;
 int undo_score=0;
-class play
-{
+class play{
 	int g[4][4];
 	int g_copy[4][4];
 	void initialize();
@@ -38,8 +37,7 @@ class play
 	void play_game();
 };
 
-void play :: instructions()
-{
+void play :: instructions(){
 	cout<<"\nInstructions for playing 2048 are:: \n"<<endl;
 	cout<<"For moving tiles enter \nw-move up/na-move left\nd-move right\ns-move down\n"<<endl;
 	cout<<"When two tiles with same number touch, they merge into one. \nWhen 2048 is created, the player wins!\n"<<endl;
@@ -47,15 +45,13 @@ void play :: instructions()
 	display();
 }
 
-int play :: random_index(int x)
-{
+int play :: random_index(int x){
 	int index;
 	index=rand()%x + 0;
 	return index;
 }
 
-void play :: lose_display()
-{
+void play :: lose_display(){
 	system("clear");
 	cout<<"\t\t\tGAME OVER\n\n";
 	cout<<"Your final score is "<<score<<"\n\n";
@@ -64,29 +60,23 @@ void play :: lose_display()
 	exit(0);
 }
 
-void play :: restart()
-{
+void play :: restart(){
 	char ch;
 	cout<<"\nAre you sure to restart the game??\n\n";
 	cout<<"enter y to restart and n to continue.\n\n";
 	cin>>ch;
-	if(ch=='y')
-	{
+	if(ch=='y'){
 		score=0;
 		undo_score=0;
 		initialize();
 	}
 }
 
-int play :: check_full()
-{
+int play :: check_full(){
 	int full_flag=1;
-	for(int i=0;i<4;i++)
-	{
-		for(int j=0;j<4;j++)
-		{
-			if(g[i][j]==0)
-			{
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4;j++){
+			if(g[i][j]==0){
 				full_flag=0;
 				break;
 			}
@@ -95,30 +85,24 @@ int play :: check_full()
 	return full_flag;
 }
 
-void play :: win_display()
-{
+void play :: win_display(){
 	char ch;
 	cout<<"\t\t\tYOU WON!!!\n\n";
 	cout<<"Your total score is "<<score<<"\n\n";
 	cout<<"Do you wish to continue???\n";
 	cout<<"Enter y to continue and n to quit\n\n";
 	cin>>ch;
-	if(ch=='n')
-	{
+	if(ch=='n'){
 		end_display();
 	}
 }
 
-int play :: calculate_max()
-{
+int play :: calculate_max(){
 	int i,j;
 	int max=0;
-	for(i=0;i<4;i++)
-	{
-		for(j=0;j<4;j++)
-		{
-			if(g[i][j]>max)
-			{
+	for(i=0;i<4;i++){
+		for(j=0;j<4;j++){
+			if(g[i][j]>max){
 				max=g[i][j];
 			}
 		}
@@ -126,8 +110,7 @@ int play :: calculate_max()
 	return max;
 }
 
-void play :: end_display()
-{
+void play :: end_display(){
 	system("clear");
 	cout<<"\nYour final score is :: "<<score<<endl<<endl;
 	cout<<"Thanks for trying!!!\n\n";
@@ -136,66 +119,47 @@ void play :: end_display()
 	exit(0);
 }
 
-int play :: game_ends()
-{
+int play :: game_ends(){
 	int i,j,flag=1;
-
-		for(i=0;i<4;i++)
-		{
-			for(j=0;j<3;j++)
-			{
-				if(g[i][j]==g[i][j+1])
-				{
+	for(i=0;i<4;i++){
+		for(j=0;j<3;j++){
+			if(g[i][j]==g[i][j+1]){
+				flag=0;
+				break;
+			}
+		}
+		if(flag==0){
+				break;
+		}
+	}
+	if(flag==1){
+		for(i=0;i<3;i++){
+			for(j=0;j<4;j++){
+				if(g[i][j]==g[i+1][j]){
 					flag=0;
 					break;
 				}
 			}
-			if(flag==0)
-			{
+			if(flag==0){
 				break;
 			}
 		}
-		if(flag==1)
-		{
-			for(i=0;i<3;i++)
-			{
-				for(j=0;j<4;j++)
-				{
-					if(g[i][j]==g[i+1][j])
-					{
-						flag=0;
-						break;
-					}
-				}
-				if(flag==0)
-				{
-					break;
-				}
-			}
-		}
-
+	}
 	return flag;
 }
 
 
-void play :: generate_new_index()
-{
+void play :: generate_new_index(){
 	int flag=1;
-	if(!check_full())
-	{
-		while(flag)
-		{
+	if(!check_full()){
+		while(flag){
 			int i=random_index(4);
 			int j=random_index(4);
-			if(g[i][j]==0)
-			{
+			if(g[i][j]==0){
 				int y=rand()%10+0;
-				if(y<6)
-				{
+				if(y<6){
 					g[i][j]=2;
-				}
-				else
-				{
+				}else{
 					g[i][j]=4;
 				}
 				flag=0;
@@ -204,12 +168,9 @@ void play :: generate_new_index()
 	}
 }
 
-void play :: initialize()
-{
-	for(int i=0;i<4;i++)
-	{
-		for(int j=0;j<4;j++)
-		{
+void play :: initialize(){
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4;j++){
 			g[i][j]=0;
 			g_copy[i][j]=0;
 		}
@@ -224,18 +185,12 @@ void play :: initialize()
 }
 
 
-void play :: move_up()
-{
-	for(int i=0;i<4;i++)
-	{
-		for(int j=0;j<4;j++)
-		{
-			if(!g[j][i])
-			{
-				for(int k=j+1;k<4;k++)
-				{
-					if(g[k][i])
-					{
+void play :: move_up(){
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4;j++){
+			if(!g[j][i]){
+				for(int k=j+1;k<4;k++){
+					if(g[k][i]){
 						g[j][i]=g[k][i];
 						g[k][i]=0;
 						break;
@@ -244,21 +199,14 @@ void play :: move_up()
 			}
 		}
 	}
-
 }
 
-void play :: move_down()
-{
-	for(int i=0;i<4;i++)
-	{
-		for(int j=3;j>=0;j--)
-		{
-			if(!g[j][i])
-			{
-				for(int k=j-1;k>=0;k--)
-				{
-					if(g[k][i])
-					{
+void play :: move_down(){
+	for(int i=0;i<4;i++){
+		for(int j=3;j>=0;j--){
+			if(!g[j][i]){
+				for(int k=j-1;k>=0;k--){
+					if(g[k][i]){
 						g[j][i]=g[k][i];
 						g[k][i]=0;
 						break;
@@ -269,42 +217,28 @@ void play :: move_down()
     }
 
 }
-void play :: move_left()
-{
-	for(int i=0;i<4;i++)
-	{
-		for(int j=0;j<4;j++)
-		{
-			if(!g[i][j])
-			{
-				for(int k=j+1;k<4;k++)
-				{
-					if(g[i][k])
-					{
+void play :: move_left(){
+	for(int i=0;i<4;i++){
+		for(int j=0;j<4;j++){
+			if(!g[i][j]){
+				for(int k=j+1;k<4;k++){
+					if(g[i][k]){
 						g[i][j]=g[i][k];
 						g[i][k]=0;
 						break;
 					}
 				}
 			}
-
 		}
 	}
-
 }
 
-void play :: move_right()
-{
-	for(int i=0;i<4;i++)
-	{
-		for(int j=3;j>=0;j--)
-		{
-			if(!g[i][j])
-			{
-				for(int k=j-1;k>=0;k--)
-				{
-					if(g[i][k])
-					{
+void play :: move_right(){
+	for(int i=0;i<4;i++){
+		for(int j=3;j>=0;j--){
+			if(!g[i][j]){
+				for(int k=j-1;k>=0;k--){
+					if(g[i][k]){
 						g[i][j]=g[i][k];
 						g[i][k]=0;
 						break;
@@ -316,68 +250,49 @@ void play :: move_right()
 
 }
 
-void play :: sum_up()
-{
-	for(int i=0;i<4;i++)
-	{
-		for(int j=0;j<3;j++)
-		{
-			if(g[j][i] && g[j][i]==g[j+1][i])
-						{
-							g[j][i]=g[j][i] + g[j+1][i];
-							g[j+1][i]=0;
-							score+=g[j][i];
-							undo_score+=g[j][i];
-						}
+void play :: sum_up(){
+	for(int i=0;i<4;i++){
+		for(int j=0;j<3;j++){
+			if(g[j][i] && g[j][i]==g[j+1][i]){
+				g[j][i]=g[j][i] + g[j+1][i];
+				g[j+1][i]=0;
+				score+=g[j][i];
+				undo_score+=g[j][i];
+			}
 		}
 	}
-
 }
 
-void play :: sum_down()
-{
-	for(int i=0;i<4;i++)
-	{
-		for(int j=3;j>0;j--)
-		{
-					if(g[j][i] && g[j][i]==g[j-1][i])
-					{
-						g[j][i]=g[j][i] + g[j-1][i];
-						g[j-1][i]=0;
-						score+=g[j][i];
-						undo_score+=g[j][i];
-					}
+void play :: sum_down(){
+	for(int i=0;i<4;i++){
+		for(int j=3;j>0;j--){
+			if(g[j][i] && g[j][i]==g[j-1][i]){
+				g[j][i]=g[j][i] + g[j-1][i];
+				g[j-1][i]=0;
+				score+=g[j][i];
+				undo_score+=g[j][i];
+			}
 		}
 	}
-
 }
 
-void play :: sum_left()
-{
-	for(int i=0;i<4;i++)
-	{
-		for(int j=0;j<3;j++)
-		{
-			if(g[i][j] && g[i][j]==g[i][j+1])
-						{
-							g[i][j]=g[i][j] + g[i][j+1];
-							g[i][j+1]=0;
-							score+=g[i][j];
-							undo_score+=g[i][j];
-						}
+void play :: sum_left(){
+	for(int i=0;i<4;i++){
+		for(int j=0;j<3;j++){
+			if(g[i][j] && g[i][j]==g[i][j+1]){
+				g[i][j]=g[i][j] + g[i][j+1];
+				g[i][j+1]=0;
+				score+=g[i][j];
+				undo_score+=g[i][j];
+			}
 		}
 	}
-
 }
 
-void play :: sum_right()
-{
-	for(int i=0;i<4;i++)
-	{
-		for(int j=3;j>0;j--)
-		{
-			if(g[i][j] && g[i][j]==g[i][j-1])
-			{
+void play :: sum_right(){
+	for(int i=0;i<4;i++){
+		for(int j=3;j>0;j--){
+			if(g[i][j] && g[i][j]==g[i][j-1]){
 				g[i][j]=g[i][j] + g[i][j-1];
 				g[i][j-1]=0;
 				score=score + g[i][j];
@@ -385,32 +300,24 @@ void play :: sum_right()
 			}
 		}
 	}
-
 }
 
-void play :: play_game()
-{
+void play :: play_game(){
 	int flag=0;
 	char choice,ch;
 	initialize();
 	cin>>choice;
 
-
-	while((choice=='w' || choice=='a' || choice=='s' || choice=='d' || choice=='q' || choice=='i' || choice=='u' || choice=='r'))
-	{
+	while((choice=='w' || choice=='a' || choice=='s' || choice=='d' || choice=='q' || choice=='i' || choice=='u' || choice=='r')){
 		//make copy
-		if(choice!='u')
-		{
-			for(int m=0;m<4;m++)
-			{
-				for(int n=0;n<4;n++)
-				{
+		if(choice!='u'){
+			for(int m=0;m<4;m++){
+				for(int n=0;n<4;n++){
 					g_copy[m][n]=g[m][n];
 				}
 			}
 		}
-	switch(choice)
-	{
+	switch(choice){
 		case 'w':
 			undo_score=0;
 			move_up();
@@ -450,8 +357,7 @@ void play :: play_game()
 		case 'q':
 			cout<<"Are you sure you want to quit??\nEnter y to quit and n to continue!\n"<<endl;
 			cin>>ch;
-			if(ch=='y')
-			{
+			if(ch=='y' || ch == 'Y'){
 				end_display();
 			}
 			display();
@@ -463,12 +369,9 @@ void play :: play_game()
 			restart();
 			break;
 		case 'u':
-			if(undo_flag<5)
-			{
-				for(int m=0;m<4;m++)
-				{
-					for(int n=0;n<4;n++)
-					{
+			if(undo_flag<5){
+				for(int m=0;m<4;m++){
+					for(int n=0;n<4;n++){
 						g[m][n]=g_copy[m][n];
 					}
 				}
@@ -476,49 +379,38 @@ void play :: play_game()
 				undo_flag++;
 				system("clear");
 				display();
-			}
-			else
-			{
+			}else{
 				system("clear");
 				display();
 				cout<<"\n\nYou cannot undo the matrix more than 5 times.\n\nSorry!!!\n"<<endl;
 			}
 	}
 	int find_max=calculate_max();
-	if(find_max==2048)
-	{
+	if(find_max==2048){
 		win_display();
 	}
-		if(check_full())
-		{
-
-			if(game_ends())
-			{
+		if(check_full()){
+			if(game_ends()){
 				lose_display();
 			}
 		}
 		cout<<"enter choice: "<<endl;
 		cin>>choice;
-		while(choice!='w'  && choice!='s'  && choice!='d'  && choice!='a' && choice!='q' && choice!='i' && choice!='u' && choice!='r')
-		{
+		while(choice!='w'  && choice!='s'  && choice!='d'  && choice!='a' && choice!='q' && choice!='i' && choice!='u' && choice!='r'){
 			cout<<"\nYou had entered the wrong choice!\nPlease enter correct choice to continue!"<<endl;
 			cin>>choice;
 		}
-
 	}
 }
 
 
-void play :: display()
-{
-	cout<<"\n\t\t\t 2048\n\n";
-	cout<<"\t\t  A msdeep14 CREATION\n\n";
+void play :: display(){
+	cout<<"\n\t\t\t\t\t\t\t 2048\n\n";
+	cout<<"\t\t\t\t\t\t  A msdeep14 CREATION\n\n";
 	cout<<"  score :: "<<score<<endl<<endl;
-	for(int i=0;i<4;i++)
-	{
+	for(int i=0;i<4;i++){
 		cout<<"                       ";
-		for(int j=0;j<4;j++)
-		{
+		for(int j=0;j<4;j++){
 			cout<<setw(8)<<g[i][j]<<setw(8)<<"|"<<setw(8);
 		}
 		cout<<endl<<endl<<endl;
@@ -527,8 +419,7 @@ void play :: display()
 	cout<<"\t\t\t\t\t\t\t\t\tr-restart\n\tw\t\t\t\t^\t\t\t\ti-instructions\na\ts\td\t\t<\t"<<"v"<<"\t>\t\t\tq-quit  u-undo\n\n";
 }
 
-int main()
-{
+int main(){
 	play p;
 	srand(time(NULL));
 	p.play_game();
